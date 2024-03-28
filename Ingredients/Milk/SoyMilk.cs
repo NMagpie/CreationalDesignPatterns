1d6b275d47026ba2e64eb10647a2c4455907ad95
NMagpie <sorochin.work@gmail.com>
2024-03-28 14:40:37 +0200

    SLIGHT REFACTOR

--- Ingredients/Milk/SoyMilk.cs
+++ Ingredients/Milk/SoyMilk.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _14._Creational_Design_Patterns.Ingredients.Milk
+﻿namespace _14._Creational_Design_Patterns.Ingredients.Milk
 {
     public class SoyMilk : BaseMilk
     {
--- Ingredients/Sugar.cs
+++ Ingredients/Sugar.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _14._Creational_Design_Patterns.Ingredients
+﻿namespace _14._Creational_Design_Patterns.Ingredients
 {
     public class Sugar
     {
